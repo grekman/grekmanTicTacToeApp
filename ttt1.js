c6b1bf226a5e0a629e790ea17f3b579a4f7b3d8c
grekman <grekman@gmail.com>
2017-04-18 01:01:13 +0300

    who win or tie, onclick

--- ttt1.js
+++ ttt1.js
@@ -3,8 +3,19 @@ var activePlayer = "X";
 var moves = [];
 var ticTacToeCells = document.getElementsByClassName("box");
 
+var winningCombinations = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [6,4,2],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8]
+];
 
-function draw(cellIndex) {
+
+function playTurn(cellIndex) {
   if (isBlank(cellIndex)) {
     // Track each player's moves
     moves[cellIndex] = activePlayer;
@@ -14,8 +25,24 @@ function draw(cellIndex) {
     } else {
       drawO(cellIndex);
     }
+
+    if (isWinner(activePlayer)) {
+      gameOver(activePlayer + " has won!");
+    } else if (isTie()) {
+      gameOver("It's a draw, try again!");
+    } else {
     activePlayer = (activePlayer === "X") ? "O" : "X";
   }
+  }
+}
+
+function gameOver(message) {
+  // Pause the game for a 1/2 second before displaying the game over message
+  setTimeout(function() {
+    alert(message);
+    reset();
+    activePlayer = (activePlayer === "X") ? "O" : "X";
+  }, 500);
 }
 
 function drawX(cellIndex) {
@@ -34,6 +61,34 @@ function isBlank(cellIndex) {
   }
 }
 
+function isWinner(player) {
+  for (var i = 0; i < winningCombinations.length; i++) {
+    if (moves[winningCombinations[i][0]] == player &&
+       moves[winningCombinations[i][1]] == player &&
+       moves[winningCombinations[i][2]] == player) {
+       highlightWinningCombo(winningCombinations[i]);
+       return true;
+    }
+  }
+  return false;
+}
+
+function isTie() {
+  for (var i = 0; i < 9; i++) {
+    console.log("moves[" + i + "]=" + moves[i]);
+    if (typeof moves[i] == 'undefined' || null === moves[i] ) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function highlightWinningCombo(winningCombo) {
+  for (var i = 0; i < winningCombo.length; i++) {
+    var cell = ticTacToeCells[winningCombo[i]];
+    cell.classList.add("winner");
+  }
+}
 
 function reset() {
   for (var i = 0; i <= moves.length; i++) {
@@ -52,14 +107,3 @@ function reset() {
 gameController.addEventListener("click", function() {
   reset();
 });
-
-for (var i = 0; i < ticTacToeCells.length; i++) {
-    addClickHandler(ticTacToeCells[i], i);
-}
-
-function addClickHandler(box, index) {
-    console.log("type", box);
-    box.addEventListener("click", function() {
-        draw(index);
-    }, false);
-}
